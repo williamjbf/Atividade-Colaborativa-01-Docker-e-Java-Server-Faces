2ac0edbc845f6ef97e25e14cd668c291590a4a9e
williamjbf <williamjbf@gmail.com>
2022-08-21 15:16:51 -0300

    corrigido bug na EditoraController

--- src/main/java/controller/EditoraController.java
+++ src/main/java/controller/EditoraController.java
@@ -4,10 +4,18 @@ import model.Editora;
 import repository.EditoraRepository;
 import repository.implement.EditoraImplementEmMemoria;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateful;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import java.util.List;
+
+@Named
+@RequestScoped
 public class EditoraController {
     private Editora editora = new Editora();
     private String busca = "";
-
+    private List<Editora> editorasEncontrada;
     private EditoraRepository repository = new EditoraImplementEmMemoria();
 
     public String salvar() {
@@ -15,4 +23,27 @@ public class EditoraController {
         return "listar";
     }
 
+    public List<Editora> listar(){
+        return repository.listarEditora();
+    }
+
+    @PostConstruct
+    public void init(){
+        this.editorasEncontrada = repository.listarEditora(); //lazy
+    }
+    public Editora getEditora() {
+        return editora;
+    }
+
+    public void setEditora(Editora editora) {
+        this.editora = editora;
+    }
+
+    public String getBusca() {
+        return busca;
+    }
+
+    public void setBusca(String busca) {
+        this.busca = busca;
+    }
 }
