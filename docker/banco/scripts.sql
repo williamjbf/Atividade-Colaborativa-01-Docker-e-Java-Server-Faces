a511da5a48fe20764e698e2b878ea790ac7cb9ff
williamjbf <williamjbf@gmail.com>
2022-08-20 21:42:02 -0300

    projeto preparado para usar o jsf

---
+++ docker/banco/scripts.sql
@@ -0,0 +1,10 @@
+class Livro{
+private int id;
+private String titulo;
+private LocalDate dataDeLancamento;
+}
+class Editora{
+private int codigo;
+private String localDeOrigem;
+private String nomeFantasia;
+}
---
+++ run.sh
@@ -0,0 +1,3 @@
+mvn clean package
+
+docker-compose -f ./docker/docker-compose.yaml up --build -d
